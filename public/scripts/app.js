e89c85654b842cdb362a33f25e43d11dfa815b93
BreakfastAtKyrils <kyrildr@gmail.com>
2022-05-04 15:30:45 -0400

    add calendar code to app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,19 @@
 // Client facing scripts here
+$(document).ready(function () {
+  //calendar code
+  $("#choose-time").flatpickr({
+    enableTime: true,
+    inline: true,
+    //ask mentor about this line
+    // "plugins": [new confirmDatePlugin({})],
+    dateFormat: "F, d Y H:i",
+    // mode: "multiple",
+
+
+  });
+  // instance.config.onChange.push(function() { } );
+
+
+
+
+});
